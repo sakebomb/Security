5a95be9067ed08a60c2425ba47517d115eedea74
Jonathan Tomek <jonathan.tomek@gmail.com>
2015-03-02 15:48:35 -0500

    Initial Commit
    Signed-off-by: Jonathan Tomek <jonathan.tomek@gmail.com>

---
+++ security_company_name_generator.py
@@ -0,0 +1,38 @@
+import random
+
+
+fname = [
+    "Threat",
+    "Intel",
+    "Cyber",
+    "Trust",
+    "Thrust",
+    "Looking",
+    "i",
+    "Mal",
+    "Proof",
+    "Fire",
+    "Phish"
+    ]
+lname = [
+    "Squad",
+    "Wave",
+    "Glass",
+    "Networks",
+    "Secure",
+    "Sight",
+    "Watch",
+    "Recon",
+    "Vision",
+    "Point",
+    "X",
+    "Security",
+    "Soft",
+    "Alliance",
+    "Exchange",
+    "Eye",
+    "Labs",
+    "House"
+    ]
+
+print random.choice(fname) + random.choice(lname)
